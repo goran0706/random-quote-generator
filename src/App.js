25d2b20fe69806b7a828f0b0b505d92ec2ef8c03
GGavrovski <goran.gavrovski@gmail.com>
2022-04-28 06:18:06 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import 'antd/dist/antd.min.css';
 import './App.css';
+import { Content } from 'antd/lib/layout/layout';
+import { Layout, Button } from 'antd';
+import { ReadOutlined } from '@ant-design/icons';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [quote, setQuote] = useState(null);
+	const [author, setAuthor] = useState(null);
+	const [error, setError] = useState(null);
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setError(null);
+		}, 3000);
+	}, [error]);
+
+	const getQuote = async () => {
+		try {
+			setLoading(true);
+
+			const options = {
+				method: 'GET',
+				url: process.env.REACT_APP_ENV_API,
+				headers: {
+					'X-RapidAPI-Host': process.env.REACT_APP_ENV_API_HOST,
+					'X-RapidAPI-Key': process.env.REACT_APP_ENV_API_KEY,
+				},
+			};
+
+			const { data } = await axios.request(options);
+			setQuote(data.content);
+			setAuthor(data.originator.name);
+			setLoading(false);
+		} catch (error) {
+			setError(error);
+			setLoading(false);
+		}
+	};
+
+	return (
+		<Layout className='wrapper'>
+			<Content className='quote'>
+				{!error && quote && (
+					<div className='quote__text'>
+						&quot;{quote}&quot;
+						{author && <div className='quote_s_text'>- {author}</div>}
+					</div>
+				)}
+				{error && <span className='quote__text'>{error.message}</span>}
+				<Button
+					className='quote__btn'
+					type='primary'
+					loading={loading}
+					onClick={getQuote}>
+					Get Quote
+					<ReadOutlined />
+				</Button>
+			</Content>
+		</Layout>
+	);
+};
 
 export default App;
