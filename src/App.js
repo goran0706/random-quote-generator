879a2653043b00196cdebd2c6dec800c05703b72
GGavrovski <goran.gavrovski@gmail.com>
2022-07-07 23:24:41 +0200

    add shoutout

--- src/App.js
+++ src/App.js
@@ -60,6 +60,17 @@ const App = () => {
 					<ReadOutlined />
 				</Button>
 			</Content>
+
+			<span className='shoutout'>
+				Photo by{' '}
+				<a href='https://unsplash.com/@alfonsmc10?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText'>
+					Alfons Morales
+				</a>{' '}
+				on{' '}
+				<a href='https://unsplash.com/s/photos/library?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText'>
+					Unsplash
+				</a>
+			</span>
 		</Layout>
 	);
 };
